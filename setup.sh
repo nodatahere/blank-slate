409f01171545349e319d17273022202535659838
no_data_here <error.404.no.data.here@gmail.com>
2018-06-01 17:40:14 -0500

    added flatpak

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list; #spotify repo
 wget -q "http://deb.playonlinux.com/public.gpg" -O- | sudo apt-key add -; #playonlinux keyserv
 sudo wget http://deb.playonlinux.com/playonlinux_xenial.list -O /etc/apt/sources.list.d/playonlinux.list; #playonlinux repo
+sudo add-apt-repository ppa:alexlarsson/flatpak; #add flatpak ppa
 #curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -; #docker gpg key
 #sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"; #add docker repo
 
@@ -24,7 +25,7 @@ sudo apt install python3; #make sure additional prereqs are installed
 #install programs I use on all my systems
 discordfile=/home/$USER/Downloads/discord-0.0.5.deb; #set this to the path of your discord .deb file which you downloaded previously because discord's site lacks a direct download link that one can wget
 
-sudo apt install -y git google-drive-ocamlfuse spotify-client playonlinux steam vlc redshift redshift-gtk lshw-gtk libreoffice; #desktop programs
+sudo apt install -y git google-drive-ocamlfuse spotify-client playonlinux steam vlc redshift redshift-gtk lshw-gtk libreoffice flatpak; #desktop programs
 sudo apt install -y x11vnc ssh; #remote access programs
 
 #programs that lack repos but have .deb files
